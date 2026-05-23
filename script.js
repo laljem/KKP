5353781bf680e5069ef69cf84a96d12326d5d248
Haulaljem <hlaljem@gmail.com>
2026-05-23 23:16:42 +0530

    initialTEst1

---
+++ script.js
@@ -0,0 +1,25 @@
+function openImage(src){
+
+  document.getElementById("lightbox").style.display = "flex";
+
+  document.getElementById("lightbox-img").src = src;
+
+}
+
+function closeImage(){
+
+  document.getElementById("lightbox").style.display = "none";
+
+}
+
+const hero = document.querySelector("header");
+
+document.addEventListener("mousemove", (e) => {
+
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;
+
+    hero.style.backgroundPosition =
+    `${50 + x * 10}% ${50 + y * 10}%`;
+
+});
